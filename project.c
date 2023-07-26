5e2ac00b08bc870f4dc8e6f79f4c7c639084b1a0
itsmohiful <mohiful.islam.ofc@gmail.com>
2023-07-27 02:27:01 +0600

    create user, login, bank account registration feature added

--- project.c
+++ project.c
@@ -1,6 +1,7 @@
 // header file
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
 // #include <conio.h>
 //#include <windows.h>
@@ -14,13 +15,13 @@
 #define BANK_DB "bankDB.txt"
 
 
-//set cursor position
-COORD coord = {0, 0};
-void gotoxy(int a, int b) {
-    coord.X = a;
-    coord.Y = b;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
+// //set cursor position
+// COORD coord = {0, 0};
+// void gotoxy(int a, int b) {
+//     coord.X = a;
+//     coord.Y = b;
+//     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+// }
 
 //user structure(obj)
 typedef struct{
@@ -31,13 +32,65 @@ typedef struct{
 
 //account structure(obj)
 typedef struct {
-    char name[USERNAME_LENGTH];
+    char name[30];
     char phone[PHONENUMBER_LENGTH];
     char email[EMAIL_LENGTH];
     char address[100];
     int accountNo;
-    char citizen[50];
 } Account;
+
+
+
+// store bank user account data when crate new bank account
+int storeBankUserData(Account userAccount){
+    if(userAccount.name && userAccount.email && userAccount.accountNo && userAccount.phone && userAccount.address){
+        FILE *bankDb = fopen(BANK_DB, "a");
+
+        if (bankDb == NULL){
+            printf("Failed to open Bank DB File.\n");
+            exit(1);
+        }
+
+        fprintf(bankDb, "%s %d %s %s %s \n",userAccount.name, userAccount.accountNo, userAccount.email, userAccount.phone, userAccount.address);
+
+        fclose(bankDb);
+        
+
+        return 1;
+    }
+
+    return 0;
+
+}
+
+void createBankAccount(){
+    Account bankAccount;
+
+    printf("Enter Your Name: ");
+    scanf("%s", &bankAccount.name);
+
+    printf("Enter Your Email: ");
+    scanf("%s", &bankAccount.email);
+
+    printf("Enter Your Phone No: ");
+    scanf("%s", &bankAccount.phone);
+
+    printf("Enter Your Address: ");
+    scanf("%s", &bankAccount.address);
+    
+    srend(time(0));
+    bankAccount.accountNo = rand() % 9000 + 1000;
+
+    if(!storeBankUserData(bankAccount)){
+        printf("Some Went Wrong! Please Try Again.");
+        createBankAccount();
+    };
+
+    printf("\n Bank Account Craeted Successfully.\n");
+    printf("\n Your Account No: %d", bankAccount.accountNo);
+    printf("\n Please Remember Your Account No For Future.\n");
+    
+};
 
 
 
@@ -47,23 +100,38 @@ int bankModule(){
     int choiceCase;
 
     //selected by authorized user(maybe after login)
-    gotoxy(48, 4);  printf("WELCOME TO MAIN MENU");
-    gotoxy(44, 8);  printf("[1] . View Customer Accounts");
-    gotoxy(44, 9);  printf("[2] . Customer Account Registration");
-    gotoxy(44, 10); printf("[3] . Edit Customer Account");
-    gotoxy(44, 11); printf("[4] . Delete Customer Account");
-    gotoxy(44, 12); printf("[5] . Search Customer Account");
-    gotoxy(44, 13); printf("[6] . Transaction");
-    gotoxy(44, 14); printf("[7] . Log Out !!! ");
-    gotoxy(44, 15); printf("[8] . About US ");
-    gotoxy(43, 20); printf("Please Enter Your Choice [1-8] : ");
+    // gotoxy(48, 4);  printf("WELCOME TO MAIN MENU");
+    // gotoxy(44, 8);  printf("[1] . View Customer Accounts");
+    // gotoxy(44, 9);  printf("[2] . Customer Account Registration");
+    // gotoxy(44, 10); printf("[3] . Edit Customer Account");
+    // gotoxy(44, 11); printf("[4] . Delete Customer Account");
+    // gotoxy(44, 12); printf("[5] . Search Customer Account");
+    // gotoxy(44, 13); printf("[6] . Transaction");
+    // gotoxy(44, 14); printf("[7] . Log Out !!! ");
+    // gotoxy(44, 15); printf("[8] . About US ");
+    // gotoxy(43, 20); printf("Please Enter Your Choice [1-8] : ");
 
-    gotoxy(43, 20); scanf("%d", &choiceCase);   //take choice input from user
+    // gotoxy(43, 20); scanf("%d", &choiceCase);   //take choice input from user
+
+
+    printf("\nWELCOME TO MAIN MENU\n");
+    printf("[1] . Customer Account Registration\n");
+    printf("[2] . View Customer Accounts\n");
+    printf("[3] . Edit Customer Account\n");
+    printf("[4] . Delete Customer Account\n");
+    printf("[5] . Search Customer Account\n");
+    printf("[6] . Transaction\n");
+    printf("[7] . About US \n");
+    printf("[8] . Log Out !!! \n");
+    printf("\n Please Enter Your Choice [1-8] : ");
+
+    // gotoxy(43, 20); scanf("%d", &choiceCase);
+    scanf("%d",&choiceCase);
 
     switch (choiceCase){
     case 1:
-        printf("Under Development");
-        break;
+        createBankAccount();
+        bankModule();
     case 2:
         printf("Under Development");
         break;
@@ -96,7 +164,7 @@ int bankModule(){
         break;
     }
 
-    return;
+    return 0;
 }
 
 
@@ -128,7 +196,7 @@ int isUsernameAlreadyExist(char username[USERNAME_LENGTH]){
 
 
 // store new user data when create new user
-void storeAuthData(authData){
+void storeAuthData(AuthUser authData){
 
     FILE *authDB = fopen(AUTH_DB, "a");
 
@@ -159,7 +227,8 @@ void createUser(){
     scanf("%s", user.password);
 
     storeAuthData(user);    //store new user information
-    printf("User Created successfully.\n");
+    printf("\nUser Created And Logged In Successfully.\n");
+    bankModule();
 }
 // ==================END CREATE USER OR REGISTER USER============================
 
@@ -168,7 +237,7 @@ void createUser(){
 // ==================START LOGIN FUNCTIONALITY============================
 
 // check user data match in auth db in login
-int authValidation(authenticateUser){
+int authValidation(AuthUser authenticateUser){
     FILE *file = fopen(AUTH_DB, "r");
 
     if (file == NULL){
@@ -206,7 +275,9 @@ void login(){
         login();
     }
 
+    printf("\n*----***----***----***----* \n");
     printf("Authentication successful.\n");
+    printf("*----***----***----***----* \n \n");
 
 }
 // ================== END LOGIN FUNCTIONALITY============================
@@ -214,15 +285,15 @@ void login(){
 
 
 // home function defination
-void home(){
+int home(){
 
     int choiceCase;
 
     //selected by annonymous user(before login)
-    gotoxy(48, 4); printf("[1] . Login \n [2] . Create User \n [3] . Exit \n");
-    gotoxy(48, 4); printf("Please Enter Your Choice [1-3] : ");
+    printf(" [1] . Login \n [2] . Create User \n [3] . Exit \n");
+    printf("Please Enter Your Choice [1-3] : ");
 
-    gotoxy(48, 4); scanf("%d", &choiceCase);    //take choice input from user
+    scanf("%d", &choiceCase);    //take choice input from user
 
 
     switch(choiceCase){
@@ -249,7 +320,7 @@ void home(){
 int main()
 {
 
-    printf(" \"Banking Solution\" \n");
+    printf(" \t \"Banking Solution\" \n");
 
     home();
 
