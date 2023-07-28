2513a6f983809f786f32c3f8f1477cb7e38610fa
itsmohiful <mohiful.islam.ofc@gmail.com>
2023-07-28 19:12:11 +0600

    design modification

--- project.c
+++ project.c
@@ -25,26 +25,126 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-
-
-
-// void gotoxy(int x, int y) {
-//     COORD coord;
-//     coord.X = x;
-//     coord.Y = y;
-//     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-// }
-
-int getTerminalRows() {
+//-----GOBAL METHOS-----------
+int getTerminalRows() {    // chatch curson position
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
     return w.ws_row;
 }
 
-
-void gotoxy(int x, int y) {
+void gotoxy(int x, int y) {     //set cusron position in x & y axis
     printf("\033[%d;%dH", y, x);
 }
+
+void gotox(int x) {      //set cusron position in x axis
+    printf("\033[%dG", x);
+}
+
+void clearTerminal(){      ///clear terminal & show project name
+    printf("\033[2J");
+    gotoxy(50,1);printf(ANSI_COLOR_MAGENTA"-------------------------------\n");
+    gotox(49); printf("|");
+    printf(ANSI_COLOR_CYAN " \t Banking Solution \t");printf(ANSI_COLOR_MAGENTA"|\n");
+    gotox(50);printf(ANSI_COLOR_MAGENTA"-------------------------------\n" ANSI_COLOR_RESET);
+    gotoxy(0,8);
+}
+
+void loading(){
+    gotoxy(30,12);printf(ANSI_COLOR_YELLOW "[LOADING....]" ANSI_COLOR_RESET);
+    sleep(2);
+
+    // printf("\033[2J");
+    gotoxy(50,1);printf(ANSI_COLOR_MAGENTA"-------------------------------\n");
+    gotox(49); printf("|");
+    printf(ANSI_COLOR_CYAN " \t Banking Solution \t");printf(ANSI_COLOR_MAGENTA"|\n");
+    gotox(50);printf(ANSI_COLOR_MAGENTA"-------------------------------\n");
+    gotoxy(0,8);
+
+}
+
+
+void successMsg(char message[300]){     //success message showing.
+    int line = strlen(message);
+    // gotoxy(1,8);
+
+    gotox(30);
+    for(int i = 0; i < line + 10; i++){printf(ANSI_COLOR_GREEN"-");};printf("\n");
+    gotox(28); printf("|");
+    printf("\t %s \t",message);
+    printf("| \n");
+    gotox(30); 
+    for(int i = 0; i < line + 10; i++){printf("-");};
+    printf("\n\n");
+    printf(""ANSI_COLOR_RESET);
+}
+
+void errorMsg(char message[300]){     //error message showing.
+    int line = strlen(message);
+    // gotoxy(1,8);
+
+    gotox(30);
+    for(int i = 0; i < line + 10; i++){printf(ANSI_COLOR_RED"-");};printf("\n");
+    gotox(28); printf("|");
+    printf("\t %s \t",message);
+    printf("| \n");
+    gotox(30); 
+    for(int i = 0; i < line + 10; i++){printf("-");};
+    printf("\n\n");
+    printf(""ANSI_COLOR_RESET);
+}
+
+
+void infoMsg(char message[300]){     //info message showing.
+    int line = strlen(message);
+    // gotoxy(1,8);
+
+    gotox(30);
+    for(int i = 0; i < line + 10; i++){printf(ANSI_COLOR_CYAN"-");};printf("\n");
+    gotox(28); printf("|");
+    printf("\t %s \t",message);
+    printf("| \n");
+    gotox(30); 
+    for(int i = 0; i < line + 10; i++){printf("-");};
+
+    printf("\n\n");
+    printf(""ANSI_COLOR_RESET);
+}
+
+void infoMsgWithPerameter(char message[300], int value){     //info message showing.
+    int line = strlen(message);
+    // gotoxy(1,8);
+
+    gotox(30);
+    for(int i = 0; i < line + 16; i++){printf(ANSI_COLOR_CYAN"-");};printf("\n");
+    gotox(28); printf("|");
+    printf("\t %s : %d\t", message, value);
+    printf("| \n");
+    gotox(30); 
+    for(int i = 0; i < line + 16; i++){printf("-");};
+
+    printf("\n\n");
+    printf(""ANSI_COLOR_RESET);
+}
+
+void warningMsg(char message[300]){     //warning message showing.
+    int line = strlen(message);
+    // gotoxy(1,8);
+
+    gotox(30);
+    for(int i = 0; i < line + 10; i++){printf(ANSI_COLOR_YELLOW"-");};printf("\n");
+    gotox(28); printf("|");
+    printf("\t %s \t",message);
+    printf("| \n");
+    gotox(30); 
+    for(int i = 0; i < line + 10; i++){printf("-");};
+
+    printf("\n\n");
+    printf(""ANSI_COLOR_RESET);
+}
+
+//-----GOBAL METHOS END-----------
+
+
 
 //user structure(obj)
 typedef struct{
@@ -64,13 +164,14 @@ typedef struct {
 
 
 
-// store bank user account data when crate new bank account
+// store bank user account data when create new bank account
 int storeBankUserData(Account userAccount){
     if(userAccount.name && userAccount.email && userAccount.accountNo && userAccount.phone && userAccount.address){
         FILE *bankDb = fopen(BANK_DB, "a");
 
         if (bankDb == NULL){
-            printf(ANSI_COLOR_RED "Failed to open Bank DB File.\n" ANSI_COLOR_RESET);
+            char msg[300] = "Failed To Open Bank DB";
+            errorMsg(msg);
             exit(1);
         }
 
@@ -86,99 +187,133 @@ int storeBankUserData(Account userAccount){
 
 }
 
+//bank account create method
 void createBankAccount(){
+
+    clearTerminal();
+
     Account bankAccount;
+    printf(ANSI_COLOR_MAGENTA "Enter Your Correct Information For Opening An Account ! \n\n " ANSI_COLOR_RESET);
 
-    int c;
-    int i = 0;
-
+    //input object value from user
     printf(ANSI_COLOR_MAGENTA "Enter Your Name: " ANSI_COLOR_RESET);
-    scanf("%29s",&bankAccount.name);
-
+    scanf("%29s",bankAccount.name);
 
     printf(ANSI_COLOR_MAGENTA "\nEnter Your Email: "ANSI_COLOR_RESET);
-    scanf("%29s",&bankAccount.email);
+    scanf("%29s",bankAccount.email);
 
     printf(ANSI_COLOR_MAGENTA "\nEnter Your Phone No: "ANSI_COLOR_RESET);
-    scanf("%15s",&bankAccount.phone);
+    scanf("%15s",bankAccount.phone);
 
     printf(ANSI_COLOR_MAGENTA "\nEnter Your Address: "ANSI_COLOR_RESET);
-    scanf("%99s",&bankAccount.address);
+    scanf("%99s",bankAccount.address);
     
+    //auto set account number
     srand(time(0));
     bankAccount.accountNo = rand() % 9000 + 1000;
 
+    //store bank account data
     if(!storeBankUserData(bankAccount)){
-        printf(ANSI_COLOR_RED "Some Went Wrong! Please Try Again." ANSI_COLOR_RESET);
+        char message[300] = "Something Went Wrong! Please Try Again";
+        errorMsg(message);
         createBankAccount();
     };
 
-    printf(ANSI_COLOR_GREEN "\n \"%s\" , Your Bank Account Successfully Craeted. \n\n"ANSI_COLOR_RESET, bankAccount.name);
-    printf(ANSI_COLOR_BLUE "\n Your Account No: %d" ANSI_COLOR_RESET, bankAccount.accountNo);
-    printf(ANSI_COLOR_YELLOW"\n -|- Please Remember Your Account No For Future -|-\n"ANSI_COLOR_RESET);
+    //showing messages
+    char mesg1[300] =  "Congratulations! Your Bank Account Created Successfully";
+    char mesg2[300] = "Yor Account Number Is ";
+    char mesg3[300] = "Please Remember Your Account Number For Future!";
+
+    successMsg(mesg1);
+    infoMsgWithPerameter(mesg2, bankAccount.accountNo);
+    warningMsg(mesg3);
+    
     
 };
 
+
+//Bank user list methods
 int bankUserList(){
+
     printf("\033[2J");
+    clearTerminal(); //clear terminal
+    printf("dhukce");
+    // return 1;
+
     Account userList;
     
     FILE *bankDB = fopen(BANK_DB, "r");
 
     if (bankDB == NULL){
-        printf(ANSI_COLOR_RED "Error: Could not open Bnak DB file.\n"ANSI_COLOR_RESET);
-        // exit(1);
+
+        char msg[300] = "Error: Could not open Bnak DB file";
+        errorMsg(msg);
+        
         return 0;
     }
 
     int listNo = 0;
-    int getRowNum = getTerminalRows();
+    int getRowNum = 4;
     char name[USERNAME_LENGTH];
     int accountNo;
     char email[EMAIL_LENGTH];
     long long int phone;
     char address[100];
 
+    //read bank data form bank DB file and store to these varriable
     while(fscanf(bankDB, "%s %d %s %lld %s\n", name, &accountNo, email, &phone, address) == 5){
 
         listNo++;
 
         if(listNo == 1){
-            printf(ANSI_COLOR_BLUE "\n \t <> All Bank Account List -- \n \n\n" ANSI_COLOR_RESET);
+
+            char msg[300] = "All Bank Account List";
+            infoMsg(msg);
             
+            //header for list table
             gotoxy(5, getRowNum);  printf(ANSI_COLOR_CYAN "Name");
             gotoxy(20, getRowNum);  printf("Account No");
             gotoxy(40, getRowNum); printf("Email");
             gotoxy(80, getRowNum); printf("Phone");
-            gotoxy(105, getRowNum); printf("Address");
+            gotoxy(105, getRowNum); printf("Address\n");
 
-
-            printf("\n------------------------------------------------------------------------------------------------------\n\n");
+            for(int i=0; i < 130; i++){printf("-");};   // under line for header
+            printf("\n");
+            
         };
-
-            getRowNum = getTerminalRows();
+            //showing table row as each user account    
+            getRowNum += 3;
             gotoxy(5, getRowNum);  printf(ANSI_COLOR_MAGENTA "%s",name);
             gotoxy(20, getRowNum);  printf("%d",accountNo);
             gotoxy(40, getRowNum); printf("%s",email);
             gotoxy(80, getRowNum); printf("%lld",phone);
-            gotoxy(105, getRowNum); printf("%s",address);
-        printf("\n------------------------------------------------------------------------------------------------------\n \n"ANSI_COLOR_RESET);
+            gotoxy(105, getRowNum); printf("%s \n",address);
+
+            for(int i=0; i < 130; i++){printf("-"ANSI_COLOR_RESET);}; // under line for header
+            printf("\n");
     };
 
     if(listNo > 0){
-        printf(ANSI_COLOR_YELLOW"\n <|> Total Active Account : %d <|>\n \n "ANSI_COLOR_RESET, listNo);
+        printf("\n\n");
+        gotox(120);
+
+        char msg[300] = "Total Active Account " ;
+
+        infoMsgWithPerameter(msg, listNo);
+    
     }
 
     if(listNo == 0){
-        printf(ANSI_COLOR_RED"\n Bank Account Not Found !\n\n" ANSI_COLOR_RESET);
+        printf("\n\n");
+        gotox(120);
+        char msg[300] = "Bank Account Not Found";
+        errorMsg(msg);
         return 0;
     }
 
     fclose(bankDB);
 
-    
-
-    return bankUserList;
+    return bankUserList; //return total account
 }
 
 
@@ -189,22 +324,7 @@ int bankModule(){
 
     int choiceCase;
 
-    //selected by authorized user(maybe after login)
-    // gotoxy(48, 4);  printf("WELCOME TO MAIN MENU");
-    // gotoxy(44, 8);  printf("[1] . View Customer Accounts");
-    // gotoxy(44, 9);  printf("[2] . Customer Account Registration");
-    // gotoxy(44, 10); printf("[3] . Edit Customer Account");
-    // gotoxy(44, 11); printf("[4] . Delete Customer Account");
-    // gotoxy(44, 12); printf("[5] . Search Customer Account");
-    // gotoxy(44, 13); printf("[6] . Transaction");
-    // gotoxy(44, 14); printf("[7] . Log Out !!! ");
-    // gotoxy(44, 15); printf("[8] . About US ");
-    // gotoxy(43, 20); printf("Please Enter Your Choice [1-8] : ");
-
-    // gotoxy(43, 20); scanf("%d", &choiceCase);   //take choice input from user
-
-
-    printf(ANSI_COLOR_GREEN "\n<> MAIN MENU <>\n \n");
+    printf(ANSI_COLOR_BLUE "\n<> MAIN MENU <>\n \n");
 
     printf(ANSI_COLOR_YELLOW "[1] . Bank Account Registration\n");
     printf("[2] . View ALL Customers Account\n");
@@ -219,50 +339,63 @@ int bankModule(){
 
     // gotoxy(43, 20); scanf("%d", &choiceCase);
     scanf("%d",&choiceCase);
+    // if(choiceCase){
+    //     loading();
+    // }
 
     switch (choiceCase){
-    case 1:{
-        createBankAccount();
-        bankModule();
-    }
-    case 2:{
-        int totalAccount = bankUserList();
-        printf("\n");
-        bankModule();
-        break;
-    }
 
-    case 3:
-        printf("Under Development");
-        break;
-
-    case 4:
-        printf("Under Development");
-        break;
-
-    case 5:
-        printf("Under Development");
-        break;
-
-    case 6:
-        printf("Under Development");
-        break;
-
-    case 7:
-        printf("Under Development");
-        break;
-
-    case 8:
-        printf("Under Development");
-        break;
-
-    default:
-        if(choiceCase > 0 && choiceCase <= 9){
+        case 1:{    //crate bank account
+            createBankAccount();
             bankModule();
-        }else{
-            printf("\n Alert: Enter Numeric Value Must! \n \n");
         }
-        break;
+        case 2:{     //account List
+            clearTerminal();
+            sleep(1);
+            bankUserList();
+            printf("\n");
+            sleep(1);
+            bankModule();
+            break;
+        }
+
+        case 3:
+            printf("Under Development");
+            break;
+
+        case 4:
+            printf("Under Development");
+            break;
+
+        case 5:
+            printf("Under Development");
+            break;
+
+        case 6:
+            printf("Under Development");
+            break;
+
+        case 7:
+            printf("Under Development");
+            break;
+
+        case 8:{
+            clearTerminal();
+            successMsg("Log Out Successfull");
+            home();
+            break;
+        }
+
+        default:
+            if(choiceCase > 0 && choiceCase <= 9){
+                bankModule();
+            }
+            else{
+                char msg[300] = "Alert: Enter Numeric Value Must!";
+                errorMsg(msg);
+                printf("\n \n");
+            }
+            break;
     }
 
     return 0;
@@ -276,8 +409,8 @@ int isUsernameAlreadyExist(char username[USERNAME_LENGTH]){
     FILE *db = fopen(AUTH_DB, "r");
 
     if (db == NULL){
-        printf("Failed to open Auth DB File.\n");
-        exit(1);
+        char msg[300] = "Failed to open Auth DB File";
+        errorMsg(msg);
     }
 
     char dbUsername[USERNAME_LENGTH];
@@ -302,7 +435,9 @@ void storeAuthData(AuthUser authData){
     FILE *authDB = fopen(AUTH_DB, "a");
 
     if (authDB == NULL){
-        printf("Failed to open Auth DB File.\n");
+
+        char msg[300] = "Failed to open Auth DB File";
+        errorMsg(msg);
         exit(1);
     }
 
@@ -322,7 +457,10 @@ void createUser(){
 
     if (isUsernameAlreadyExist(user.username)){     //showing error if username already exist in db
 
-        printf(ANSI_COLOR_RED "\nAlert: Username already exist ! Try again -\n\n");
+        
+        char msg[300] = "Alert: Username already exist ! Try again";
+        errorMsg(msg);
+        printf("\n\n");
         createUser();
     }
 
@@ -330,8 +468,14 @@ void createUser(){
     scanf("%29s", user.password);
 
     storeAuthData(user);    //store new user information
-    printf(ANSI_COLOR_GREEN "\n  -- User Created And Logged In Successfully -- \n\n");
-    printf(ANSI_COLOR_MAGENTA "\n  -|- Welcome \"%s\" , To Banking Solution -|- \n\n"ANSI_COLOR_RESET, user.username);
+
+    char msg[300] = "User Created And Logged In Successfully";
+    successMsg(msg);
+
+    char msg1[300] = "Welcome To Banking Solution";
+    infoMsg(msg1);
+
+
     bankModule();
 }
 // ==================END CREATE USER OR REGISTER USER============================
@@ -345,7 +489,8 @@ int authValidation(AuthUser authenticateUser){
     FILE *file = fopen(AUTH_DB, "r");
 
     if (file == NULL){
-        printf(ANSI_COLOR_RED "Failed to open Auth DB File.\n" ANSI_COLOR_RESET);
+        char msg[300] = "Failed to open Auth DB File !";
+        errorMsg(msg);
         exit(1);
     }
 
@@ -376,19 +521,18 @@ void login() {
     printf(ANSI_COLOR_CYAN "\nEnter password: ");
     scanf("%29s", authenticateUser.password);
 
-    // Consume the newline character left in the input buffer
-    // while ((blankChar = getchar()) != '\n' && blankChar != EOF);
-
     if (!authValidation(authenticateUser)) {
-        printf("\nInvalid username or password! Try again.\n");
+        printf("\n\n");
+        char message[300] = "Invalid username or password! Try again ";
+        errorMsg(message);
+        printf("\n \n");
+
         login();
     }
-    printf("\033[2J"); 
-    // fflush(stdout);
+    clearTerminal(); //clear terminal
 
-    printf(ANSI_COLOR_BLUE "\n*----***----***----***----*\n");
-    printf(ANSI_COLOR_MAGENTA "Authentication successful.\n");
-    printf(ANSI_COLOR_BLUE "*----***----***----***----*\n\n"ANSI_COLOR_RESET);
+    char message[300] = "Authentication successful";
+    successMsg(message);
 }
 
 // ================== END LOGIN FUNCTIONALITY============================
@@ -406,6 +550,8 @@ int home(){
 
     scanf("%d", &choiceCase);    //take choice input from user
     printf("\n");
+    // loading();
+    
 
     switch(choiceCase){
         case 1:{
@@ -434,14 +580,9 @@ int home(){
 
 
 // main function
-int main()
-{
-    // printf(ANSI_COLOR_RED "This is red text.\n" ANSI_COLOR_RESET);
-
-    printf(ANSI_COLOR_CYAN " \t \"Banking Solution\" \n \n" ANSI_COLOR_RESET);
-
+int main(){
+    clearTerminal(); //showing project name & clean terminal
     home();
-
 
     return 0;
 }
